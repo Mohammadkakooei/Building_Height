1fd0014ce2fa42d19d7ecbce8d054698798255c9
Mohammad Kakooei <50989054+Mohammadkakooei@users.noreply.github.com>
2024-09-29 20:56:35 +0200

    Update Export_patches.js

--- Data/Export_patches.js
+++ Data/Export_patches.js
@@ -1,42 +1,73 @@
 var S1Collection = ee.ImageCollection("COPERNICUS/S1_GRD"),
     VIIRSCollection = ee.ImageCollection("NOAA/VIIRS/DNB/MONTHLY_V1/VCMSLCFG"),
-    imageCollection = ee.ImageCollection("users/Temporary/Copernicus_Land_Monitoring_Service");
+    imageCollection = ee.ImageCollection("users/Temporary/Copernicus_Land_Monitoring_Service"),
+    S2Collection = ee.ImageCollection("COPERNICUS/S2");
 
 var HeightMap =  imageCollection.mosaic().rename('Height');
 Map.addLayer(HeightMap,{},'HeightMap',false);
 
+var imgList = imageCollection.toList(38);
+
+for (var i=0; i<38; i++)
+{
+  
+  var img = ee.Image(imgList.get(i));
+  var img_id = ee.String(img.get('system:index')).getInfo();
+  var img_geo = img.geometry();
+  
+  var img2 = img.focal_min(2500, 'square','meters');
+  img2 = img2.where(img.lt(500),img);
+
 var S1Collection2 = S1Collection  // ASCENDING
-                    .filterBounds(geometry)
+                    .filterBounds(img_geo)
                     .filterDate('2020-01-01','2021-01-01')
+                    .filter(ee.Filter.listContains('transmitterReceiverPolarisation','VH'))
                     .select('VV','VH');
-var S1img = S1Collection2.mean().clip(geometry);
-Map.addLayer(S1img,{},'S1img',false);
 
-var S2_CNN_collection = ee.ImageCollection("users/ghsl/S2_CNN");
-var S2_CNN_img = S2_CNN_collection.mosaic().clip(geometry).gt(30).rename('U0');
-Map.addLayer(S2_CNN_img,{}, 'S2_CNN_img', false);
+var S1img_ASC = S1Collection2.filter(ee.Filter.eq('orbitProperties_pass','ASCENDING')).mean().clip(img_geo).rename('VV_A','VH_A');
+//Map.addLayer(S1img_ASC,{},'S1img_ASC',false);
 
+var S1img_DES = S1Collection2.filter(ee.Filter.eq('orbitProperties_pass','DESCENDING')).mean().clip(img_geo).rename('VV_D','VH_D');
+//Map.addLayer(S1img_DES,{},'S1img_DES',false);
+
+var S1img = ee.Image.cat([S1img_ASC, S1img_DES]);
+//Map.addLayer(S1img,{},'S1img',false);
+ 
 var VIIRSCollection2 = VIIRSCollection
                     .filterDate('2020-01-01','2021-01-01')
-                    .filterBounds(geometry) ;
-var NL_img = VIIRSCollection2.mean().select('avg_rad').clip(geometry).rename('NL');
-Map.addLayer(NL_img,{},'NL_img',false);
+                    .filterBounds(img_geo) ;
+var NL_img = VIIRSCollection2.mean().select('avg_rad').clip(img_geo).rename('NL');
+//Map.addLayer(NL_img,{},'NL_img',false);
 
+ var terrain = ee.call('Terrain', ee.Image('USGS/SRTMGL1_003'));
+var elevation = terrain.select(['elevation']).clip(img_geo);
+//Map.addLayer(elevation,{},'elevation',false);
                              
-var InputImg = ee.Image.cat([S1img, S2_CNN_img, NL_img, HeightMap2.unmask(0)]).clip(geometry);                             
-Map.addLayer(InputImg,{},'InputImg',false);
+var InputImg = ee.Image.cat([S1img, NL_img, elevation, img2]).clip(img_geo);                             
+Map.addLayer(InputImg,{},'InputImg_'+img_id,false);
 
+var image_export_options2 = {
+  'fileDimensions': [224, 224],
+  'shardSize': 224,
+  'cloudOptimized': true
+}
 
-Export.image.toDrive({
+Export.image.toCloudStorage({
   image: InputImg.float(),
-  description: 'Rome_Podgorica_Height',
-  folder: 'EuropHeight2',
-  region: geometry,
+  description: img_id,
+  bucket: 'heightmap',
+  fileNamePrefix: 'Train03/'+img_id+'/',
   scale: 10,
+  maxPixels:1e13, 
+  region: img_geo,
   crs: 'EPSG:4326',
-  maxPixels: 1e13,
-  shardSize: 224,
-  fileDimensions: [224, 224], 
-  skipEmptyTiles: true
-  
+  fileFormat: 'GeoTIFF',
+  skipEmptyTiles: true,
+  formatOptions : image_export_options2
 })
+}
+
+
+
+
+
